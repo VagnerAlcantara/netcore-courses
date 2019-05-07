732566e31775a1ea51e615c819cbe0769c43eb79
VagnerAlcantara <vagnerjosedea.ferreira@outlook.com>
2019-05-07 08:39:06 -0300

    Aula 05 - Finalizada.

--- FabianoNalin/FN.Store/FN.Store.Api/Startup.cs
+++ FabianoNalin/FN.Store/FN.Store.Api/Startup.cs
@@ -1,6 +1,7 @@
 ﻿using FN.Store.DI;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace FN.Store.Api
@@ -9,12 +10,25 @@ namespace FN.Store.Api
     {
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().AddJsonOptions(options =>
-            {
-                options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
-                options.SerializerSettings.NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore;
-            });
+            services.AddMvc
+                (
+                    x =>
+                    {
+                        //Força a api retornar um erro 406 caso seja socilitado um tipo (ex. xml) que não é aceito pela api.
+                        x.ReturnHttpNotAcceptable = true;
+                        x.OutputFormatters.Add(new XmlDataContractSerializerOutputFormatter());
+                    }
+                )
+                .AddJsonOptions(options =>
+                {
+                    options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+                    options.SerializerSettings.NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore;
+                });
             services.AddDependencies();
+            services.AddSwaggerGen(s =>
+            {
+                s.SwaggerDoc("v1", new Swashbuckle.AspNetCore.Swagger.Info() { Title = "Store API", Version = "v1" });
+            });
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
@@ -24,6 +38,12 @@ namespace FN.Store.Api
                 app.UseDeveloperExceptionPage();
             }
             app.UseMvc();
+            app.UseSwagger();
+            app.UseSwaggerUI(x =>
+            {
+                x.SwaggerEndpoint("swagger/v1/swagger.json", "Store API");
+                x.RoutePrefix = string.Empty;
+            });
         }
     }
 }
