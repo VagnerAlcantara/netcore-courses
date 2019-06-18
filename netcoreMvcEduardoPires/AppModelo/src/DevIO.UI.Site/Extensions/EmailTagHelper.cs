0153cb65a9fc2e7340d795385e23198e0d976384
Vagner José de Alcantara Ferreira <vagnerjosedea.ferreira@outlook.com>
2019-06-18 19:09:25 -0300

    Bundling and Minification + custom tag helpers

---
+++ netcoreMvcEduardoPires/AppModelo/src/DevIO.UI.Site/Extensions/EmailTagHelper.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevIO.UI.Site.Extensions
+{
+    public class EmailTagHelper : TagHelper
+    {
+        public string EmailDomain { get; set; } = "desenvolvedor.io";
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            // informo que será um link
+            output.TagName = "a";
+            //recupera o conteúdo que ficara dentro da taghelper
+            var content = await output.GetChildContentAsync();
+            var target = content.GetContent() + "@" + EmailDomain;
+            output.Attributes.SetAttribute("href", "mailto:" + target);
+            output.Content.SetContent(target);
+        }
+    }
+}
