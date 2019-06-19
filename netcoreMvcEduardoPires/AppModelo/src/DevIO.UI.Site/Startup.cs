3120a118574432cc3afc4a8877c67d969046f8f5
Vagner José de Alcantara Ferreira <vagnerjosedea.ferreira@outlook.com>
2019-06-19 11:52:28 -0300

    Iniciando com áreas.

--- netcoreMvcEduardoPires/AppModelo/src/DevIO.UI.Site/Startup.cs
+++ netcoreMvcEduardoPires/AppModelo/src/DevIO.UI.Site/Startup.cs
@@ -30,9 +30,8 @@ namespace DevIO.UI.Site
 
             app.UseMvc(routes =>
             {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                routes.MapRoute(name: "default", template: "{controller=Home}/{action=Index}/{id?}");
+                routes.MapRoute(name: "area", template: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
