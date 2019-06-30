fc56f43060923cbfc31137fd2cab0c069ca10877
Vagner José de Alcantara Ferreira <vagnerjosedea.ferreira@outlook.com>
2019-06-30 11:59:24 -0300

    Customizando autenticação.

--- netcoreMvcEduardoPires/AspNetCoreIdentity/AspNetCoreIdentity/Controllers/HomeController.cs
+++ netcoreMvcEduardoPires/AspNetCoreIdentity/AspNetCoreIdentity/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using AspNetCoreIdentity.Models;
 using Microsoft.AspNetCore.Authorization;
+using AspNetCoreIdentity.Extensions;
 
 namespace AspNetCoreIdentity.Controllers
 {
@@ -17,25 +18,36 @@ namespace AspNetCoreIdentity.Controllers
         {
             return View();
         }
+
         public IActionResult Privacy()
         {
             return View();
         }
+
         [Authorize(Roles = "Admin")]
         public IActionResult Secret()
         {
             return View();
         }
+
         [Authorize(Policy = "PodeExcluir")]
         public IActionResult SecretClaim()
         {
             return View("Secret");
         }
+
         [Authorize(Policy = "PodeEscrever")]
         public IActionResult SecretClaimGravar()
         {
             return View("Secret");
         }
+
+        [ClaimsAutorizeAttribute("Produtos", "Ler")]
+        public IActionResult ClaimsCustom()
+        {
+            return View("Secret");
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
