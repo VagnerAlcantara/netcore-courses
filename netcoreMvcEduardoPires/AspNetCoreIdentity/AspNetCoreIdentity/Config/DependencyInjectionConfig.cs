63a560c5962e1ab5650d2f70d2837bbf40355686
Vagner José de Alcantara Ferreira <vagnerjosedea.ferreira@outlook.com>
2019-07-13 18:04:55 -0300

    Início App Mvc Basica (sem camadas)

--- netcoreMvcEduardoPires/AspNetCoreIdentity/AspNetCoreIdentity/Config/DependencyInjectionConfig.cs
+++ netcoreMvcEduardoPires/AspNetCoreIdentity/AspNetCoreIdentity/Config/DependencyInjectionConfig.cs
@@ -17,6 +17,9 @@ namespace AspNetCoreIdentity.Config
             {
                 return Logger.Factory.Get();
             });
+
+            services.AddScoped<AuditoriaFilter>();
+
             return services;
         }
     }
