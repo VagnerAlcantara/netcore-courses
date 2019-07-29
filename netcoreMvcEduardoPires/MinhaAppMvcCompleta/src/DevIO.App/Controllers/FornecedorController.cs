2a5f567b910a4ac5e53132023d6e85d384637e14
VagnerAlcantara <vagnerjosedea.ferreira@outlook.com>
2019-07-29 08:34:57 -0300

    Utilizando Modal Window de edição

--- netcoreMvcEduardoPires/MinhaAppMvcCompleta/src/DevIO.App/Controllers/FornecedorController.cs
+++ netcoreMvcEduardoPires/MinhaAppMvcCompleta/src/DevIO.App/Controllers/FornecedorController.cs
@@ -12,11 +12,13 @@ namespace DevIO.App.Controllers
     public class FornecedorController : BaseController
     {
         private readonly IFornecedorRepository _fornecedorRepository;
+        private readonly IEnderecoRepository _enderecoRepository;
         private readonly IMapper _mapper;
 
-        public FornecedorController(IFornecedorRepository fornecedorRepository, IMapper mapper)
+        public FornecedorController(IFornecedorRepository fornecedorRepository, IEnderecoRepository enderecoRepository, IMapper mapper)
         {
             _fornecedorRepository = fornecedorRepository;
+            _enderecoRepository = enderecoRepository;
             _mapper = mapper;
         }
 
@@ -120,6 +122,37 @@ namespace DevIO.App.Controllers
             return RedirectToAction(nameof(Index));
         }
 
+        public async Task<IActionResult> AtualizarEndereco(Guid id)
+        {
+            var fornecedor = await ObterFornecedorEndereco(id);
+
+            if (fornecedor == null) return NotFound();
+
+            return PartialView("_AtualizarEndereco", new FornecedorViewModel { Endereco = fornecedor.Endereco });
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AtualizarEndereco(FornecedorViewModel fornecedorViewModel)
+        {
+            ModelState.Remove("Nome");
+            ModelState.Remove("Documento");
+            if (!ModelState.IsValid) return PartialView("_AtualizarEndereco", fornecedorViewModel);
+
+            await _enderecoRepository.Atualizar(_mapper.Map<Endereco>(fornecedorViewModel.Endereco));
+
+            var url = Url.Action("ObterEndereco", "Fornecedores", new { id = fornecedorViewModel.Endereco.FornecedorId });
+
+            return Json(new { success = true, url });
+        }
+
+        public async Task<IActionResult> ObterEndereco(Guid id)
+        {
+            var fornecedor = await ObterFornecedorEndereco(id);
+
+            if (fornecedor == null) return NotFound();
+
+            return PartialView("_DetalhesEndereco", fornecedor);
+        }
         private async Task<FornecedorViewModel> ObterFornecedorEndereco(Guid id)
         {
             return _mapper.Map<FornecedorViewModel>(await _fornecedorRepository.ObterFornecedorEndereco(id));
--- netcoreMvcEduardoPires/MinhaAppMvcCompleta/src/DevIO.App/wwwroot/js/site.js
+++ netcoreMvcEduardoPires/MinhaAppMvcCompleta/src/DevIO.App/wwwroot/js/site.js
@@ -1,4 +1,34 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿function ajaxModal() {
+    $(document).ready(function () {
+        $.ajaxSetup({ cache: false });
+        $("a[data-modal]").on("click", function (e) {
+            $("#myModalContent").load(this.href, function () {
+                $("#myModal").modal({
+                    keyboard: true
+                }, 'show');
+                bindForm(this);
+            });
+            return false;
+        });
+    });
 
-// Write your JavaScript code.
+    function bindForm() {
+        $('form', dialog).submit(function () {
+            $.ajax({
+                url: this.action,
+                type: this.method,
+                data: $(this).serialize(),
+                success: function (result) {
+                    if (result.success) {
+                        $("#myModal").modal('hide');
+                        $("#EnderecoTarget").load(result.url); // carrega o resultado html para a div
+                    } else {
+                        $("#myModal").html(result);
+                        bindForm(dialog);
+                    }
+                }
+            });
+            return false;
+        });
+    }
+}
