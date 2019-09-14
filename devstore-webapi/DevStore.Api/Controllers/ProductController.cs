489ad3555df8fb429fcf8c022f4d7da2016762eb
Vagner José de Alcantara Ferreira <vagnerjosedea.ferreira@outlook.com>
2019-09-14 19:43:36 -0300

    novos projetos

---
+++ devstore-webapi/DevStore.Api/Controllers/ProductController.cs
@@ -0,0 +1,164 @@
+﻿using System.Web.Http;
+using DevStore.Infra.DataContexts;
+using System.Net.Http;
+using System.Linq;
+using DevStore.Domain;
+using System.Web.Http.Cors;
+
+namespace DevStore.Api.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    [RoutePrefix("api/v1/public")]
+    public class ProductController : ApiController
+    {
+        private DevStoreDataContext db = new DevStoreDataContext();
+
+        [Route("products")]
+        public HttpResponseMessage GetProducts()
+        {
+            try
+            {
+                var result = db.Products.Include("Category").ToList();
+
+                if (result.Count > 0)
+                    return Request.CreateResponse(System.Net.HttpStatusCode.OK, result);
+                else
+                    return Request.CreateResponse(System.Net.HttpStatusCode.NoContent);
+
+
+            }
+            catch (System.Exception ex)
+            {
+                return Request.CreateResponse(System.Net.HttpStatusCode.InternalServerError, ex.Message);
+            }
+        }
+
+        [Route("categories")]
+        public HttpResponseMessage GetCategories()
+        {
+            try
+            {
+                var result = db.Categories.ToList();
+
+                if (result.Count > 0)
+                    return Request.CreateResponse(System.Net.HttpStatusCode.OK, result);
+                else
+                    return Request.CreateResponse(System.Net.HttpStatusCode.NoContent);
+
+
+            }
+            catch (System.Exception ex)
+            {
+                return Request.CreateResponse(System.Net.HttpStatusCode.InternalServerError, ex.Message);
+            }
+        }
+
+        [Route("categories/{categoryId}/products")]
+        public HttpResponseMessage GetProductsByCategory(int categoryId)
+        {
+            try
+            {
+                var result = db.Products.Include("Category").Where(x => x.CategoryId == categoryId).ToList();
+
+                if (result.Count > 0)
+                    return Request.CreateResponse(System.Net.HttpStatusCode.OK, result);
+                else
+                    return Request.CreateResponse(System.Net.HttpStatusCode.NoContent);
+
+
+            }
+            catch (System.Exception ex)
+            {
+                return Request.CreateResponse(System.Net.HttpStatusCode.InternalServerError, ex.Message);
+            }
+        }
+
+        [HttpPost]
+        [Route("products")]
+        public HttpResponseMessage PostProduct(Product product)
+        {
+            if (product == null)
+                return Request.CreateResponse(System.Net.HttpStatusCode.BadRequest);
+
+            try
+            {
+                db.Products.Add(product);
+                db.SaveChanges();
+
+                return Request.CreateResponse(System.Net.HttpStatusCode.Created, product);
+            }
+            catch (System.Exception ex)
+            {
+                return Request.CreateResponse(System.Net.HttpStatusCode.InternalServerError, "Falha ao incluir producto " + ex.Message);
+            }
+        }
+
+        [HttpPatch] // atualizar parte da classe
+        [Route("products")]
+        public HttpResponseMessage PatchProduct(Product product)
+        {
+            if (product == null)
+                return Request.CreateResponse(System.Net.HttpStatusCode.BadRequest);
+
+            try
+            {
+                db.Entry<Product>(product).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+
+                return Request.CreateResponse(System.Net.HttpStatusCode.OK, product);
+            }
+            catch (System.Exception ex)
+            {
+                return Request.CreateResponse(System.Net.HttpStatusCode.InternalServerError, "Falha ao alterar producto " + ex.Message);
+            }
+        }
+
+        [HttpPut]
+        [Route("products")]
+        public HttpResponseMessage PutProduct(Product product)
+        {
+            if (product == null)
+                return Request.CreateResponse(System.Net.HttpStatusCode.BadRequest);
+
+            try
+            {
+                db.Entry<Product>(product).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+
+                return Request.CreateResponse(System.Net.HttpStatusCode.OK, product);
+            }
+            catch (System.Exception ex)
+            {
+                return Request.CreateResponse(System.Net.HttpStatusCode.InternalServerError, "Falha ao alterar producto " + ex.Message);
+            }
+        }
+
+        [HttpDelete]
+        [Route("products")]
+        public HttpResponseMessage DeleteProduct(int productId)
+        {
+            if (productId <= 0)
+                return Request.CreateResponse(System.Net.HttpStatusCode.BadRequest);
+
+            try
+            {
+                db.Products.Remove(db.Products.Find(productId));
+                db.SaveChanges();
+
+                return Request.CreateResponse(System.Net.HttpStatusCode.OK, "Produto excluído.");
+            }
+            catch (System.Exception ex)
+            {
+                return Request.CreateResponse(System.Net.HttpStatusCode.InternalServerError, "Falha ao excluir producto " + ex.Message);
+            }
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+        }
+    }
+}
---
+++ devstore-webapi/DevStore.Infra/DataContexts/DevStoreDataContext.cs
@@ -0,0 +1,50 @@
+﻿using DevStore.Domain;
+using DevStore.Infra.Mappings;
+using System.Data.Entity;
+
+namespace DevStore.Infra.DataContexts
+{
+    public class DevStoreDataContext : DbContext
+    {
+        public DevStoreDataContext()
+            : base("DevStoreConnectionString")
+        {
+            Database.SetInitializer<DevStoreDataContext>(new DevStoreDataContextInitializer());
+            Configuration.LazyLoadingEnabled = false;
+            Configuration.ProxyCreationEnabled = false;
+        }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Configurations.Add(new ProductMap());
+            modelBuilder.Configurations.Add(new CategoryMap());
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+    public class DevStoreDataContextInitializer : DropCreateDatabaseIfModelChanges<DevStoreDataContext>
+    {
+        protected override void Seed(DevStoreDataContext context)
+        {
+            context.Categories.Add(new Category { Id = 1, Title = "Informática" });
+            context.Categories.Add(new Category { Id = 2, Title = "Games" });
+            context.Categories.Add(new Category { Id = 3, Title = "Papelaria" });
+            context.SaveChanges();
+
+            context.Products.Add(new Product() { Id = 1, CategoryId = 1, IsActive = true, Title = "Produto 1" });
+            context.Products.Add(new Product() { Id = 2, CategoryId = 1, IsActive = true, Title = "Produto 2" });
+            context.Products.Add(new Product() { Id = 3, CategoryId = 1, IsActive = true, Title = "Produto 3" });
+
+            context.Products.Add(new Product() { Id = 4, CategoryId = 2, IsActive = true, Title = "Produto 4" });
+            context.Products.Add(new Product() { Id = 5, CategoryId = 2, IsActive = true, Title = "Produto 5" });
+            context.Products.Add(new Product() { Id = 6, CategoryId = 2, IsActive = true, Title = "Produto 6" });
+
+            context.Products.Add(new Product() { Id = 7, CategoryId = 3, IsActive = true, Title = "Produto 7" });
+            context.SaveChanges();
+
+            base.Seed(context);
+        }
+    }
+}
