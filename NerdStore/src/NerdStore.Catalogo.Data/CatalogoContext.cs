81bccefff48450dc82719e48437f0603f880afb6
vagner.ferreira <vagner.ferreira@habibs.com.br>
2020-03-05 07:42:48 -0300

    Inicio da persistencia de dados.

---
+++ NerdStore/src/NerdStore.Catalogo.Data/CatalogoContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NerdStore.Catalogo.Domain;
+using NerdStore.Core.Data;
+using System;
+using System.Threading.Tasks;
+
+namespace NerdStore.Catalogo.Data
+{
+    public class CatalogoContext : DbContext, IUnitOfWork
+    {
+        //TODO: Continuar no minuto 9
+        public CatalogoContext(DbContextOptions<CatalogoContext> options) : base(options) { }
+
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+        public Task<bool> Commit()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
