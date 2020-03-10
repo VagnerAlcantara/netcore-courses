57db45d3d181c730ca8d6916b6ebc828360312be
vagner.ferreira <vagner.ferreira@habibs.com.br>
2020-03-10 07:52:52 -0300

    iniciando services

---
+++ NerdStore/src/NerdStore.Catalogo.Domain/EstoqueService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NerdStore.Catalogo.Domain
+{
+    public class EstoqueService
+    {
+    }
+    public interface IEstoqueService
+    {
+        Task<bool> DebitarEstoque(Guid produtoId, int quantidade);
+        Task<bool> ReporEstoque(Guid produtoId, int quantidade);
+    }
+}
