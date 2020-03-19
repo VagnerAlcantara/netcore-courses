056f489d5fee5d4baf5689f74007d2dfb8f2e939
Vagner José de Alcantara Ferreira <vagnerjosedea.ferreira@outlook.com>
2020-03-18 23:39:45 -0300

    Implementando Mediatr

---
+++ NerdStore/src/NerdStore.Catalogo.Domain/Events/ProdutoAbaixoEstoqueEvent.cs
@@ -0,0 +1,14 @@
+﻿using NerdStore.Core.DomainObjects;
+using System;
+
+namespace NerdStore.Catalogo.Domain.Events
+{
+    public class ProdutoAbaixoEstoqueEvent : DomainEvents
+    {
+        public int QuantidadeRestante { get; private set; }
+        public ProdutoAbaixoEstoqueEvent(Guid aggregateId, int quantidadeRestante) : base(aggregateId)
+        {
+            QuantidadeRestante = quantidadeRestante;
+        }
+    }
+}
---
+++ NerdStore/src/NerdStore.Core/DomainObjects/DomainEvents.cs
@@ -0,0 +1,13 @@
+﻿using NerdStore.Core.Messages;
+using System;
+
+namespace NerdStore.Core.DomainObjects
+{
+    public class DomainEvents : Event
+    {
+        public DomainEvents(Guid aggregateId)
+        {
+            AggregateId = aggregateId;
+        }
+    }
+}
