c22403e44f45fc10d9acc87bc52e57fda7eb2b77
Vagner José de Alcantara Ferreira <vagnerjosedea.ferreira@outlook.com>
2020-03-22 11:53:27 -0300

    Inclusão do projeto .mvc

--- NerdStore/src/NerdStore.Catalogo.Data/CatalogoContext.cs
+++ NerdStore/src/NerdStore.Catalogo.Data/CatalogoContext.cs
@@ -9,16 +9,15 @@ namespace NerdStore.Catalogo.Data
 {
     public class CatalogoContext : DbContext, IUnitOfWork
     {
-        //TODO: Continuar no minuto 9
         public CatalogoContext(DbContextOptions<CatalogoContext> options) : base(options) { }
 
         public DbSet<Produto> Produtos { get; set; }
         public DbSet<Categoria> Categorias { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            foreach (var property in modelBuilder.Model.GetEntityTypes().SelectMany(
-                e => e.GetProperties().Where(p => p.ClrType == typeof(string))))
-                property.Relational().ColumnType = "varchar(100)";
+           //foreach (var property in modelBuilder.Model.GetEntityTypes().SelectMany(
+           //    e => e.GetProperties().Where(p => p.ClrType == typeof(string))))
+           //    property.Relational().ColumnType = "varchar(100)";
 
             modelBuilder.ApplyConfigurationsFromAssembly(typeof(CatalogoContext).Assembly);
         }
